3009ebdfc121fa82ca891056bffb25251263e1cc
DevanshYadav <infodevanshyadav@gmail.com>
2026-08-25 22:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (9.80%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+       vector<string>res;
+       int n = nums.size();
+       int i;
+       for(i=0;i<n;i++){
+        int start=nums[i];
+        while(i+1<n && nums[i+1]==nums[i]+1){
+            i++;
+        }
+        int end = nums[i];
+        if(start==end){
+            res.push_back(to_string(start));
+        }
+        else{
+            res.push_back(to_string(start)+"->"+to_string(end));
+        }
+       }
+       return res;
+    }
+};
